06c8989082b876d21309ad7ac6d2eb75822a002d
Robert Knight <robert.knight@mendeley.com>
2015-04-23 07:52:34 +0100

    Remove react-testing talk
    This was moved to its own repository -
    github.com/robertknight/react-testing

--- react-testing/tests/tests.js
+++
@@ -1,8 +0,0 @@
-// this is the entry point for the test suite for use in
-// webpack
-const testModules = [
-	'./TweetItem_test',
-	'./TweetList_test'
-];
-
-testModules.forEach(testModule => require(testModule));
